7d731a59f2dd613211b1494c7a90fcd6994980ee
Steven0106 <80328557+Steven0106@users.noreply.github.com>
2021-04-01 22:33:45 +0900

    Add files via upload

---
+++ Car.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Car.h"
+using namespace std;
+
+void Car::set_power()
+{
+    power = !power;
+    return;
+}
+void Car::set_speed(bool a)
+{
+    if (!power)return;
+    if (a)
+        speed += 0.1;
+    else
+        speed -= 0.1;
+    return;
+}
+double Car::get_speed() const
+{
+    return speed;
+}
